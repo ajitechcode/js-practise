dc9cab796f12d232c9fff787916fc44543de99cf
ajitechcode <112329485+ajitechcode@users.noreply.github.com>
2023-09-27 15:43:43 +0100

    first commit

---
+++ js practice/index.js
@@ -0,0 +1,75 @@
+// let x = 20;
+// let y = 30;
+
+// let sumOfBothVariables = [x + y];
+
+// console.log(sumOfBothVariables);
+
+        /*String*/
+
+// let myName = 'Ibraahiim'
+
+// let mySurname = 'Abdurahiim'
+
+// let myFullName = ` ${myName} ${mySurname}` //myName + mySurname "string literal"
+
+// console.log(myFullName)
+
+// let num1 = '2'
+// let num2 = 6
+
+// let myAns = num1 + num2   //` ${num1}${num2}`
+
+// console.log(myAns)
+
+let MyLetter = ` Dear ...ma,
+    greeting
+    
+    conclusion
+`
+// console.log(MyLetter);
+
+// let a = 'john'
+// let b = 'peter'
+
+// let fName = a +' '+ b
+
+// console.log(fName)
+
+// let words = "there is no class today isn't".toUpperCase() + ' ' + 'hello'// or 'there is no class today isn\'t' 
+// let allCaps = words.toUpperCase()
+
+// console.log(words)
+// console.log(allCaps)
+
+// let myLetterMale = MyLetter.replace('ma', 'sir')
+
+// let conclusion = MyLetter.slice(-2)
+// let wordcount = MyLetter.length
+
+// console.log(myLetterMale)
+// console.log(conclusion)
+// console.log(wordcount)
+
+
+
+let me = 'I am learning javascript.'
+
+let b = me.toUpperCase()
+
+let s = me.slice(4)
+
+let f = me.replace('javascript', 'coding')
+
+let r = me.length
+
+let h = me.replace('javascript', 'coding').slice(4).toUpperCase()
+
+console.log(me)
+console.log(b)
+console.log(s)
+console.log(f)
+console.log(r)
+console.log(h)
+
+
